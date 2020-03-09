049aadd814f7482ac146e149667106bc63f9fc56
rizztheg <61125780+rizztheg@users.noreply.github.com>
2020-03-09 00:16:15 -0400

    Add files via upload

---
+++ midterm review.py
@@ -0,0 +1,16 @@
+class Shape:
+    def __init__(self):
+        self.boundary=[]
+    def set_boundary(boundary):
+    def __str__(self):
+        
+                
+        pass
+class Point(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "({},{})".format(self.x, self.y)
+p=Point(3,1)
+print(p)
